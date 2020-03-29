a793bb9cd43962c15759f1a2212fc4e3f0cde490
guilherme.rudi <guilherme.rudi@dbccompany.com.br>
2020-03-28 22:31:49 -0300

    implementação de evento de integracao de vendas

--- picareta.car/Administracao/src/admin.picareta/Entidades/Events/IntegrationEvents/EnviarCarroParaVendasEvent.cs
+++ picareta.car/Administracao/src/admin.picareta/Entidades/Events/IntegrationEvents/EnviarCarroParaVendasEvent.cs
@@ -5,9 +5,17 @@ namespace Admin.Picareta.Entidades.Events.IntegrationEvents
 {
     public class EnviarCarroParaVendasEvent : IntegrationEvent
     {
-        public EnviarCarroParaVendasEvent()
+        public EnviarCarroParaVendasEvent(string nomeModelo, string cor, decimal valor)
         {
-            throw new NotImplementedException();
+            NomeModelo = nomeModelo;
+            Cor = cor;
+            Valor = valor;
         }
+
+        public string NomeModelo { get; private set; }
+        public string Cor { get; private set; }
+        public decimal Valor { get; private set; }
+
+        
     }
 }
