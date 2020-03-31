9a53702c6047f545f453d732651444c13aae1288
guilherme.rudi <guilherme.rudi@dbccompany.com.br>
2020-03-30 22:37:05 -0300

    inclusão de setup depency injection in domain

---
+++ picareta.car/Administracao/src/admin.picareta/Setup/IoC/AdminDependecies.cs
@@ -0,0 +1,20 @@
+﻿using Admin.Picareta.Commands;
+using Admin.Picareta.Commands.Handlers;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Admin.Picareta.Setup.IoC
+{
+    public static class AdminDependecies
+    {
+        public static void RegisterAdminDependencies(this IServiceCollection services)
+        {
+            //Repositories
+            
+            //Commands
+            services.AddScoped<IRequestHandler<AdicionarIntencaoVendaCommand, bool>, IntencaoVendaCommandHandler>();
+
+            //IntegrationEvents
+        }
+    }
+}
